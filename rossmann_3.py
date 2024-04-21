4cc81b0d035ca8d645df5ca42447f8ad44ec4a15
Eduardo Henrique <drdhenri@gmail.com>
2024-04-21 18:01:51 -0300

    filtragem de linhas e colunas

--- rossmann_3.py
+++ rossmann_3.py
@@ -6,27 +6,46 @@ import pandas as pd
 import math
 import calendar
 import numpy as np
-import seaborn as sns
+""" import seaborn as sns
 import matplotlib.pyplot as plt
 from IPython.core.display import HTML
 from IPython.display import Image
-import datetime
+import datetime """
 
 # %%
 ## Configurando os gráficos
-def jupyter_settings():
+ def jupyter_settings():
     %matplotlib inline
     
-    plt.style.use( 'bmh' )
+    """ plt.style.use( 'bmh' )
     plt.rcParams['figure.figsize'] = [25, 12]
     plt.rcParams['font.size'] = 24
-    display( HTML( '<style>.container { width:100% !important; }</style>') )
+    display( HTML( '<style>.container { width:100% !important; }</style>') ) """
     pd.options.display.max_columns = None
     pd.options.display.max_rows = None
     pd.set_option( 'display.expand_frame_repr', False )
-    sns.set_theme(style="ticks", color_codes=True)
-jupyter_settings()
+    """ sns.set_theme(style="ticks", color_codes=True) """
+jupyter_settings() 
 
 # %%
 df3 = pd.read_csv('data/df2.csv')
+df3.drop('Unnamed: 0', axis = 1, inplace = True)
+
+# %%
+df3.head(3)
+
+# %%
+df3.shape
+# %%
+
+## Filtragem de linhas
+
+df3 = df3[(df3['open'] != 0) & (df3['sales'] > 0)]
+
+# %%
+
+## Filtragem de colunas
+cols_drop = ['open', 'customers','month_map', 'promo_interval' ]
+df3 = df3.drop(cols_drop, axis = 1)
+
 # %%
